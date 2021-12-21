38fdde26b0ffed1d298f0898e7328be52ce0883b
Scheibelr <scheibelr@gmail.com>
2021-12-21 15:24:23 -0500

    Redux Implemented!

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
-class About extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-        partners: PARTNERS
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
     };
-}
+};
+class About extends Component {
+
 
     static navigationOptions = {
         title: 'About Us'
@@ -20,7 +22,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                     />
             );
         };
@@ -29,7 +31,7 @@ class About extends Component {
             <Mission />
             <Card title= "Community Partners">
                 <FlatList
-                    data={this.state.partners}
+                    data={this.props.partners.partners}
                     renderItem={renderPartner}
                     keyExtractor={item => item.id.toString()}
                     />
@@ -44,4 +46,4 @@ function Mission () {
         <Text style={{margin: 10}}>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
     );
 }
-export default About;
+export default connect(mapStateToProps)(About);
