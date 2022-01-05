95a31ca414eb87c0ca53962dc3d84dd81e476d39
Scheibelr <scheibelr@gmail.com>
2022-01-05 16:11:53 -0500

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
@@ -68,6 +68,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -99,6 +109,14 @@ function RenderCampsite(props) {
                     raised
                     reverse
                     onPress={() => props.onShowModal()}
+                />
+                <Icon
+                    name={'share'}
+                    type='font-awesome'
+                    color='#5637DD'
+                    raised
+                    reverse
+                    onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                 />
                 </View>
             </Card>
